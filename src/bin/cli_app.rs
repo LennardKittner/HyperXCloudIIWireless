729b903c31f3a0a5f46eea6354d7446035dbbada
lennard <lennardkittner@icloud.com>
2023-06-10 12:17:45 +0200

    New CLI app

---
+++ src/bin/cli_app.rs
@@ -0,0 +1,24 @@
+use hyper_x_cloud_ii_wireless::Device;
+
+fn main() {
+    let device = match Device::new() {
+        Ok(device) => device,
+        Err(error) => {
+            eprintln!("{error}");
+            std::process::exit(1);
+        }
+    };
+    let (battery_level, charging) = match device.get_battery_level() {
+        Ok(t) => t,
+        Err(error) => {
+            eprintln!("{error}");
+            std::process::exit(1);
+        }
+    };
+    println!("Battery level: {}%", battery_level);
+    if charging {
+        println!("Charging");
+    } else {
+        println!("Not charging");
+    }
+}
