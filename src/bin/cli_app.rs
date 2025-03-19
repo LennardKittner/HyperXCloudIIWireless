5ed739038123519a3c3e92f8232e33d8ea276d83
Lennard Kittner <lennard@kittner.dev>
2025-03-19 23:41:32 +0100

    Update CLI app to use new interface

--- src/bin/cli_app.rs
+++ src/bin/cli_app.rs
@@ -1,27 +1,25 @@
-use hyper_x_cloud_ii_wireless::Device;
+use hyper_x_cloud_ii_wireless::devices::{cloud_ii_wireless_dts::CloudIIWirelessDTS, Device};
 
 fn main() {
-    let mut device = match Device::new() {
+    let mut device = match CloudIIWirelessDTS::new() {
         Ok(device) => device,
         Err(error) => {
             eprintln!("{error}");
             std::process::exit(1);
         }
     };
-    let battery_level = match device.update_battery_level() {
-        Ok(t) => t,
-        Err(error) => {
-            eprintln!("{error}");
-            std::process::exit(1);
-        }
+
+    if let Err(error) = device.refresh_state() {
+        eprintln!("{error}");
+        std::process::exit(1);
     };
-    println!("Battery level: {}%", battery_level);
+    println!("Device State: {}", device.get_device_state());
 }
 
 #[test]
 fn test_basic_device_access() {
-    let _ = match Device::new() {
+    let _ = match CloudIIWirelessDTS::new() {
         Ok(device) => device,
-        Err(_) => return
+        Err(_) => return,
     };
 }
